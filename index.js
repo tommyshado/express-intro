035bd3e7290a0f3fe8f963dae3681f7ac508eae7
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-07-20 15:33:06 +0200

    changed my the layout folder name to layouts

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { engine } from "express-handlebars"
 import express from "express";
+import { engine } from "express-handlebars"
 
 // SET UP EXPRESS server:
 
@@ -12,23 +12,22 @@ app.set("view engine", "handlebars");
 
 // render a template
 // when the application starts it will look for the "/" route then...
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
 
     // respond with the content in home.handlebars file
     res.render("home");
 });
 
 // route
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
     res.send("Bill Settings WebApp Using ExpressJS");
 });
 
 // PORT address
-let PORT = process.env.PORT || 3007;
+let PORT = process.env.PORT || 3008;
 
 // start the server
 // when the app runs it will listen for the 3007 PORT number
-app.listen(PORT, function() {
-
+app.listen(PORT, () => {
     console.log(`App is starting on port http://localhost:${PORT}`);
 });
